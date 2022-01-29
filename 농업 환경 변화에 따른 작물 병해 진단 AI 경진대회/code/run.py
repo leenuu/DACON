75310ab567dcc61ed92644fd9a4039fdad12645c
leenuu <61344683+leenuu@users.noreply.github.com>
2022-01-30 00:28:51 +0900

    Update run.py

--- 농업 환경 변화에 따른 작물 병해 진단 AI 경진대회/code/run.py
+++ 농업 환경 변화에 따른 작물 병해 진단 AI 경진대회/code/run.py
@@ -1,4 +1,14 @@
 import os
 
-os.system("python base.py 32 2e-5 512 0.4875 20 0.9 400 400 best_model.pt")
+batch_size=8
+learning_rate=25e-6
+embedding_dim=512
+dropout_rate=0.25
+epochs=30
+momentum=0.8
+img_size_x = 640
+img_size_y = 640
 
+os.system(f"python use_numworker.py {batch_size} {learning_rate} {embedding_dim} {dropout_rate} {epochs} {momentum} {img_size_x} {img_size_y}")
+
+# os.system(f"python predict.py {batch_size} {embedding_dim} {dropout_rate} {img_size_x} {img_size_y}")
