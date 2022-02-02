1d8fcc345dcbaf25d49f01f83a397ef1affb5259
Moonshinhyuk <73520866+Moonshinhyuk@users.noreply.github.com>
2022-02-02 15:44:28 +0900

    update
    good

--- 농업 환경 변화에 따른 작물 병해 진단 AI 경진대회/code/run.py
+++ 농업 환경 변화에 따른 작물 병해 진단 AI 경진대회/code/run.py
@@ -1,14 +1,22 @@
 import os
 
-batch_size=8
-learning_rate=25e-6
+batch_size=16
+learning_rate=0.000025
 embedding_dim=512
-dropout_rate=0.25
-epochs=30
+dropout_rate=0.35
+epochs=200
 momentum=0.8
-img_size_x = 640
-img_size_y = 640
+weigh_decay = 0.00005
+img_size_x = 500
+img_size_y = 500
+model = 'resnet50'
+optimizer = 'adamw'
+scheduler = 'cosine'
+rnn = "GRU"
 
-os.system(f"python use_numworker.py {batch_size} {learning_rate} {embedding_dim} {dropout_rate} {epochs} {momentum} {img_size_x} {img_size_y}")
+
+os.system(f"python base.py {batch_size} {learning_rate} {embedding_dim} {dropout_rate} {epochs} {momentum} {img_size_x} {img_size_y} {weigh_decay}")
+
 
 # os.system(f"python predict.py {batch_size} {embedding_dim} {dropout_rate} {img_size_x} {img_size_y}")
+
